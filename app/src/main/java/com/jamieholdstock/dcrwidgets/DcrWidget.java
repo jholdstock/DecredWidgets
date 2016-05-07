72078872d77dc0c84a937c99fe09f16c50d5c9e9
Jamie Holdstock <jam.jar69@gmail.com>
2016-05-07 16:57:51 +0100

    Initial commit

---
+++ app/src/main/java/com/jamieholdstock/dcrwidgets/DcrWidget.java
@@ -0,0 +1,13 @@
+package com.jamieholdstock.dcrwidgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class DcrWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
