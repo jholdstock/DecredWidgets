85e266f8888dc867d0d8b1dc8cad80ac044990fd
Jamie Holdstock <jamie.holdstock@openbet.com>
2016-05-11 10:21:07 +0100

    Revert "Revert "layout improvement""
    This reverts commit a1798d2cee80b2f00169be9592cb8aff61e5deee.

--- app/src/main/java/com/jamieholdstock/dcrwidgets/DcrWidget.java
+++ app/src/main/java/com/jamieholdstock/dcrwidgets/DcrWidget.java
@@ -15,20 +15,18 @@ import com.jamieholdstock.dcrwidgets.service.DcrStats;
 import com.jamieholdstock.dcrwidgets.service.DcrStatsService;
 
 public class DcrWidget extends AppWidgetProvider {
-    private static String currentMessage = "Last Price:";
+    private static String usdPrice = "";
+    private static String btcPrice = "";
     private static boolean refreshing = false;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int i = 0; i < appWidgetIds.length; i++) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.dcr_widget_layout);
-            views.setTextViewText(R.id.textView, currentMessage);
+            views.setTextViewText(R.id.text_btc_price, btcPrice);
+            views.setTextViewText(R.id.text_usd_price, usdPrice);
 
-            Intent intent = new Intent(context, getClass());
-            intent.setAction(MyIntents.BUTTON_PRESSED);
-            PendingIntent pi =  PendingIntent.getBroadcast(context, 0, intent, 0);
-
-            views.setOnClickPendingIntent(R.id.refreshButton, pi);
+            attachClickIntent(context, views, R.id.refreshButton);
 
             if (refreshing) {
                 views.setViewVisibility(R.id.refreshButton, View.GONE);
@@ -43,13 +41,22 @@ public class DcrWidget extends AppWidgetProvider {
         }
     }
 
+    private void attachClickIntent(Context context, RemoteViews views, int id) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(MyIntents.BUTTON_PRESSED);
+        PendingIntent pi =  PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(id, pi);
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
 
         if (action.equals(MyIntents.BUTTON_PRESSED))
         {
-            currentMessage = "...";
+            usdPrice = "...";
+            btcPrice = "...";
             refreshing = true;
             Intent msgIntent = new Intent(context, DcrStatsService.class);
             msgIntent.setAction(MyIntents.GET_STATS);
@@ -61,17 +68,22 @@ public class DcrWidget extends AppWidgetProvider {
         else if (action.equals(MyIntents.UPDATE_WIDGET)) {
             refreshing = false;
             DcrStats stats = (DcrStats) intent.getExtras().get(IntentExtras.DCR_STATS);
+
             double dUsdPrice = stats.getUsdPrice();
-            String sUsdPrice = String.format("%.2f", dUsdPrice);
-            currentMessage = " Last Price: $" + sUsdPrice;
+            usdPrice = String.format("%.2f", dUsdPrice);
+
+            double dBtcPrice = stats.getBtcPrice();
+            btcPrice = String.format("%.4f", dBtcPrice);
+
             AppWidgetManager gm = AppWidgetManager.getInstance(context);
             int[] ids = gm.getAppWidgetIds(new ComponentName(context, this.getClass()));
             this.onUpdate(context, gm, ids);
         }if (action.equals(MyIntents.UPDATE_WIDGET_ERROR)) {
             refreshing = false;
-            currentMessage = intent.getStringExtra(IntentExtras.ERROR_MESSAGE);
+            btcPrice = "";
+            usdPrice = "";
 
-            L.l("widget received error '" + currentMessage + "' from service");
+            L.l("widget received error '" + intent.getStringExtra(IntentExtras.ERROR_MESSAGE) + "' from service");
 
             AppWidgetManager gm = AppWidgetManager.getInstance(context);
             int[] ids = gm.getAppWidgetIds(new ComponentName(context, this.getClass()));
