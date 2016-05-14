c58c31e86d1d5a00f329e506fe69aed902a489d1
Jamie Holdstock <jam.jar69@gmail.com>
2016-05-14 17:47:10 +0100

    refactor to ensure listeners are attached on orientation change

--- app/src/main/java/com/jamieholdstock/dcrwidgets/widget/DcrWidget.java
+++ app/src/main/java/com/jamieholdstock/dcrwidgets/widget/DcrWidget.java
@@ -12,7 +12,6 @@ import com.jamieholdstock.dcrwidgets.MyStrings;
 import com.jamieholdstock.dcrwidgets.R;
 import com.jamieholdstock.dcrwidgets.intenthandlers.ButtonPressedHandler;
 import com.jamieholdstock.dcrwidgets.intenthandlers.DrawErrorHandler;
-import com.jamieholdstock.dcrwidgets.intenthandlers.IntentHandler;
 import com.jamieholdstock.dcrwidgets.intenthandlers.DrawStatsHandler;
 import com.jamieholdstock.dcrwidgets.intents.MyIntents;
 import com.jamieholdstock.dcrwidgets.service.DcrStatsService;
@@ -22,24 +21,6 @@ public class DcrWidget extends AppWidgetProvider {
     private static boolean waitingForService = false;
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        MyStrings.init(context);
-        this.sendIntentToService(context);
-        attachClickListeners(context, appWidgetManager, appWidgetIds);
-    }
-
-    private void attachClickListeners(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.dcr_widget_layout);
-        for (int i = 0; i < appWidgetIds.length; i++) {
-            Intent intent = new Intent(context, getClass());
-            intent.setAction(MyIntents.BUTTON_PRESSED);
-            PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
-            views.setOnClickPendingIntent(R.id.root_layout, pi);
-            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
-        }
-    }
-
-    @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (!action.contains(MyIntents.INTENT_PREFIX)) {
@@ -47,32 +28,53 @@ public class DcrWidget extends AppWidgetProvider {
             return;
         }
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.dcr_widget_layout);
-        IntentHandler handler = null;
+        RemoteViews views = getViews(context);
         switch (action) {
             case MyIntents.BUTTON_PRESSED:
                 this.sendIntentToService(context);
-                handler = new ButtonPressedHandler(intent, views);
+                new ButtonPressedHandler(intent, views).handle();
                 break;
 
             case MyIntents.DRAW_STATS:
                 waitingForService = false;
-                handler = new DrawStatsHandler(intent, views);
+                new DrawStatsHandler(intent, views).handle();
                 break;
 
             case MyIntents.DRAW_ERROR:
                 waitingForService = false;
-                handler = new DrawErrorHandler(intent, views);
+                new DrawErrorHandler(intent, views).handle();
                 break;
         }
+        
+        pushUpdatedWidget(views, context);
+    }
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] widgetIds) {
+        MyStrings.init(context);
+        this.sendIntentToService(context);
+        RemoteViews views = getViews(context);
+        pushUpdatedWidget(views, context);
+    }
+
+    private RemoteViews getViews(Context context) {
+        return new RemoteViews(context.getPackageName(), R.layout.dcr_widget_layout);
+    }
+
+    private void pushUpdatedWidget(RemoteViews views, Context context) {
         AppWidgetManager awm = AppWidgetManager.getInstance(context);
-        int[] ids = awm.getAppWidgetIds(new ComponentName(context, getClass()));
-
-        for (int i = 0; i < ids.length; i++) {
-            handler.handle();
-            awm.updateAppWidget(ids[i], views);
+        attachClickListener(context, views);
+        int[] widgetIds = awm.getAppWidgetIds(new ComponentName(context, getClass()));
+        for (int i = 0; i < widgetIds.length; i++) {
+            awm.updateAppWidget(widgetIds[i], views);
         }
+    }
+
+    private void attachClickListener(Context context, RemoteViews views) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(MyIntents.BUTTON_PRESSED);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.root_layout, pi);
     }
 
     private void sendIntentToService(Context context) {
